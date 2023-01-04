5d8156277de3585054b98e8b54b83edb894f39eb
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2023-01-04 01:42:07 -0300

    database structure correction

--- db/sql/create_tables.sql
+++ db/sql/create_tables.sql
@@ -5,6 +5,12 @@ CREATE TABLE if not exists users (
                     created_at timestamp NOT NULL
                     );
 
+-- Creating status table
+CREATE TABLE if not exists status_table (
+                    id INT NOT NULL PRIMARY KEY,
+                    status_name TEXT NOT NULL
+                    );
+
 -- Creating subscription table
 CREATE TABLE if not exists subscription (
                     id SERIAL NOT NULL  PRIMARY KEY,
@@ -12,18 +18,13 @@ CREATE TABLE if not exists subscription (
                     status_id int NOT NULL,
                     created_at timestamp NOT NULL,
                     updated_at timestamp NOT NULL,
-                    FOREIGN KEY (user_id) REFERENCES users (id)
-                    );
--- Creating status table
-CREATE TABLE if not exists status_table (
-                    id INT NOT NULL,
-                    status_name TEXT NOT NULL
-                    -- FOREIGN KEY (id) REFERENCES subscription (status_id)
+                    FOREIGN KEY (user_id) REFERENCES users (id),
+                    FOREIGN KEY (status_id) REFERENCES status_table(id)
                     );
                     
 -- Creating event_history table
 CREATE TABLE if not exists event_history (
-                    id SERIAL NOT NULL PRIMARY KEY,
+                    id SERIAL NOT NULL,
                     subscription_id INT NOT NULL,
                     type VARCHAR NOT NULL,
                     created_at timestamp NOT NULL,
