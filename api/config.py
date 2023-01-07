f9c2cc7292f2812fa474ad27f6a9ff8bd4a4c9c7
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2023-01-07 17:33:17 -0300

    kafka servers modification

--- api/config.py
+++ api/config.py
@@ -1,7 +1,7 @@
 import asyncio
 
 #environment variables
-KAFKA_BOOTSTRAP_SERVERS= "kafka:9092"
+KAFKA_BOOTSTRAP_SERVERS= "localhost:9093"
 KAFKA_TOPIC="kafka"
 KAFKA_CONSUMER_GROUP="group-id"
 loop = asyncio.get_event_loop()
