42984e66b4b5a1fe60ef6f5d5ab629ed83ed4506
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2023-01-13 04:35:49 -0300

    adding password to user schema

--- api/schema.py
+++ api/schema.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
-
+    
 class User(BaseModel):
     full_name: str
+    password: str
     
 
 class Notification(BaseModel):
     action : str
     full_name: str
+
