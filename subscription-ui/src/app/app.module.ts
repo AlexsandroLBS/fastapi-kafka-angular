09f2dd2eb166517636276d5c03dc05f12a4a4ace
AlexsandroLBS <alexsandro.lopes009@gmail.com>
2023-01-13 04:38:41 -0300

    creating angular ui

---
+++ subscription-ui/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { UserPageComponent } from './user-page/user-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    UserPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
